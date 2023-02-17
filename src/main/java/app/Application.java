657966a0539a288724243dcb854bac2d0288553c
Vadim Vagapov <midav1903@mail.ru>
2023-02-17 17:07:43 +0500

    Lesson complete

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -8,12 +8,18 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Application {
 
     public static void main(String[] args) {
+        long time = 0;
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         for (int i = 0; i < 5; i++) {
             AnimalsCage bean =
                     applicationContext.getBean(AnimalsCage.class);
             bean.whatAnimalSay();
+            if (i == 0) {
+                time = bean.getTimer().getTime();
+                continue;
+            }
+            System.out.println(time);
         }
     }
 
